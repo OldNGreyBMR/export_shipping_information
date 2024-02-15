ac7f173442eaa92924f2137766a8229a59436227
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-02-16 09:53:35 +1000

    v1.5.0.a Update ScriptedInstaller.php
    v1.5.0.a use key shipping_export2

--- v1.5.8/zc_plugins/ExportShippingInformation/v1.5.0/Installer/ScriptedInstaller.php
+++ v1.5.8/zc_plugins/ExportShippingInformation/v1.5.0/Installer/ScriptedInstaller.php
@@ -5,13 +5,13 @@ class ScriptedInstaller extends ScriptedInstallBase
 {
     protected function executeInstall()
     {
-        zen_deregister_admin_pages(['shipping_export']);
+        zen_deregister_admin_pages(['shipping_export2']);
         zen_register_admin_page(
-            'shipping_export', 'BOX_TOOLS_SHIPPING_EXPORT2', 'FILENAME_SHIPPING_EXPORT2', '', 'tools', 'Y');
+            'shipping_export2', 'BOX_TOOLS_SHIPPING_EXPORT2', 'FILENAME_SHIPPING_EXPORT2', '', 'tools', 'Y');
     }
 
     protected function executeUninstall()
     {
-        zen_deregister_admin_pages(['shipping_export']);
+        zen_deregister_admin_pages(['shipping_export2']);
     }
 }
